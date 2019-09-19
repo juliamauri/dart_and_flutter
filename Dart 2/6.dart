992d4cab53045ec147ca5e81f235b389fa85bdc2
Julià Mauri Costa <julimacowork@gmail.com>
2019-09-19 15:50:33 +0200

    Dart 2.6 done

--- Dart 2/6.dart
+++ Dart 2/6.dart
@@ -5,15 +5,27 @@ void main(){
   var content = file.readAsStringSync();
   print(content);
   List<String> lines = content.split('\n');
-  print(lines);
+  print(lines.length.toString());
+
+  //print(lines); //Prints the list bad
+
   String ret = '';
+  int linenum = 0;
   for(var line in lines)
   {
-    int indexcoma = line.indexOf(',');
-    int sum = int.parse(line.substring(0, indexcoma)) + 10;
-    line.replaceRange(0, indexcoma, sum.toString());
-    print(line);
-    ret += line + '\n';
+    if(line.isNotEmpty)
+    {
+      int indexcoma = line.indexOf(',');
+      print('Line: $linenum and indexcoma: $indexcoma');
+      if(indexcoma != -1) {
+        int sum = int.parse(line.substring(0, indexcoma)) + 10;
+        print(sum.toString());
+        ret += sum.toString() + line.substring(indexcoma, line.length);
+        if(linenum != linenum - 1)
+            ret += '\n';
+      }
+      linenum++;
+    }
   }
   print(ret);
   file.writeAsStringSync(ret);
